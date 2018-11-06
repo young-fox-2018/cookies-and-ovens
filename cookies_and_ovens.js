b518f3049c4848566403737992efd4139202bcdf
fauzan_bintang <obin.aji@gmail.com>
2018-11-06 20:35:19 +0700

    cookies-and-oven done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,35 @@
 //
 //
 // Your code here
+const Cookie = require("./Cookie");
+
+class Oven {
+    static bake(cookie, time) {
+        for (let i = 0; i < cookie.length; i++) {
+            console.log(`---------- ${cookie[i].name} ----------`);
+            for (let j = 5; j <= time; j += 5) {
+                if (j < cookie[i].time) {
+                    console.log(`${cookie[i].name}, menit ke ${j} : mentah`);
+                }
+                if (j === cookie[i].time-5) {
+                    console.log(`${cookie[i].name}, menit ke ${j} : hampir matang`);
+                }
+                if (j === cookie[i].time) {
+                    console.log(`${cookie[i].name}, menit ke ${j} : matang`);
+                }
+                if (j > cookie[i].time) {
+                    console.log(`${cookie[i].name}, menit ke ${j} : hangus`);
+                }
+            }
+        }
+    }
+}
+//  data
+var kueCoklat = new Cookie('Kue Coklat', 20);
+var kueKacang = new Cookie('Kue Kacang', 30);
+var kueKeju = new Cookie('Kue Keju', 35);
+
+//  yang mau dimasukkan ke oven
+var cookie = [kueCoklat, kueKacang, kueKeju];
+
+Oven.bake(cookie, 40);
