a52736736029e1248669dcbdbb815d2a9a7aa37b
sendyakbar <msendyakbars@gmail.com>
2018-11-06 17:16:18 +0700

    refactor, remove oven class, add bake method to cookie class, improvement on display

--- cookie.js
+++ cookie.js
@@ -3,6 +3,29 @@ class Cookie {
         this.flavor = flavor
         this.status = 'uncooked'
     }
+
+    bake(time) {
+        for (let i = 0; i <= time; i += 5) {
+            let result = `${this.flavor} cookies, ${i} min: `
+            if (i < this.cookedAt - 5) {
+                this.status = 'uncooked'
+                result += this.status
+            }
+            else if (i > this.cookedAt - 10 && i < this.cookedAt) {
+                this.status = 'almost cooked'
+                result += this.status
+            }
+            else if (i == this.cookedAt) {
+                this.status = 'cooked'
+                result += this.status
+            }
+            else if (i > this.cookedAt) {
+                this.status = 'over cooked'
+                result += this.status
+            }
+            console.log(result)
+        }
+    }
 }
 
 module.exports = Cookie
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const Peanut = require('./peanut')
 const Chocolate = require('./chocolate')
 const Cheese = require('./cheese')
-const Oven = require('./oven')
 
 let peanutCookies = new Peanut('peanut')
 let chocolateCookies = new Chocolate('chocolate')
 let cheeseCookies = new Cheese('cheese')
 
-let arr = [peanutCookies, chocolateCookies, cheeseCookies]
+let time = 35
 
-Oven.bake(arr, 35)
+console.log('========== PEANUT COOKIES ==========')
+peanutCookies.bake(time)
+console.log('')
+console.log('========= CHOCOLATE COOKIES =========')
+chocolateCookies.bake(time)
+console.log('')
+console.log('========== CHEESE COOKIES ==========')
+cheeseCookies.bake(time)
