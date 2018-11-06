591b74888a349f544c0cb9f4127386018c8bc71c
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-06 19:10:55 +0700

    final submission

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -6,7 +6,6 @@
 // - How will the classes interact with each other?
 // - Which classes will inherit from others, if any?
 //
-//
 // Your code here
 
 class Cookies {
@@ -21,25 +20,25 @@ class Microwave{
         this.name = name
     }
 
-    cook(){
+    static cook(cookie){
         console.log(`Progress ${this.name}`);
         
-        for (let i = 0; i < this.name.duration+10; i+=5) {
+        for (let i = 0; i < cookie.duration+10; i+=5) {
             if (i < 20) {
                 console.log(
-                    `${this.name.namaKue}, menit ke ${i} : Mentah`
+                    `${cookie.namaKue}, menit ke ${i} : Mentah`
                 );
             } else if (i == 20) {
                 console.log(
-                    `${this.name.namaKue}, menit ke ${i} : Mateng`
+                    `${cookie.namaKue}, menit ke ${i} : Mateng`
                 );
             } else if (i == 15) {
                 console.log(
-                    `${this.name.namaKue}, menit ke ${i} : Hampir Mateng`
+                    `${cookie.namaKue}, menit ke ${i} : Hampir Mateng`
                 );
             } else if (i > 20) {
                 console.log(
-                    `${this.name.namaKue}, menit ke ${i} : Goshongg`
+                    `${cookie.namaKue}, menit ke ${i} : Goshongg`
                 );
             }
         }
@@ -50,13 +49,11 @@ let kueCoklat = new Cookies("kue coklat", 20)
 let kueKacang = new Cookies("kue kacang", 30)
 let kueKeju = new Cookies("kue keju", 40)
 
-let make_kueCoklat = new Microwave(kueCoklat)
-let make_kueKacang = new Microwave(kueKacang)
-let make_kueKeju = new Microwave(kueKeju)
+let kue_coklat = Microwave.cook(kueCoklat)
+let kue_Kacang = Microwave.cook(kueKacang)
+let kue_keju = Microwave.cook(kueKeju)
 
-make_kueCoklat.cook()
-make_kueKacang.cook()
-make_kueKeju.cook()
+
 
 
 
