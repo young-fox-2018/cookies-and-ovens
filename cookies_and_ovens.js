4327ad3483f0d20de44232e59cd2c78f60b66d92
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-11-07 10:17:52 +0700

    fix konsep yang salah

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -24,26 +24,43 @@ class KueKeju extends Kue {
 }
 class Oven {
     constructor() {
+        this.trays = []
     }
-    bake(kue, timer) {
-        for (let i = 5; i < timer; i += 5) {
-            let output = ''
-            if (i <= kue.cooked - 10) {
-                output = `menit ke: ${i}. status: mentah`
+
+    addCookie(kue) {
+        this.trays.push(kue)
+    }
+
+    bake(timer) {
+        for (let i = 0; i < this.trays.length; i++) {
+            for (let j = 5; j < timer; j += 5) {
+                let output = `${this.trays[i].name} `
+                if (j <= this.trays[i].cooked - 10) {
+                    output += `menit ke: ${j}. status: mentah`
+                }
+                else if (j <= this.trays[i].cooked - 5) {
+                    output += `menit ke: ${j}. status: hampir matang`
+                }
+                else if (j === this.trays[i].cooked) {
+                    output += `menit ke: ${j}. status: matang`
+                }
+                else if (j >= this.trays[i].cooked + 5) {
+                    output += `menit ke: ${j}. status: hangus`
+                }
+                console.log(output)
             }
-            else if (i <= kue.cooked - 5) {
-                output = `menit ke: ${i}. status: hampir matang`
-            }
-            else if (i === kue.cooked) {
-                output = `menit ke: ${i}. status: matang`
-            }
-            else if (i >= kue.cooked + 5) {
-                output = `menit ke: ${i}. status: hangus`
-            }
-            console.log(output)
         }
     }
 
+
 }
 
-let kue1 = new Oven().bake(new KueCoklat('kue coklat'), 40)
+// let kue1 = new Oven().bake(new KueCoklat('kue coklat'), 40)
+
+let coklat = new KueCoklat('kue coklat')
+let keju = new KueKeju('kue keju')
+let oven = new Oven()
+oven.addCookie(coklat)
+oven.addCookie(keju)
+
+oven.bake(30)
